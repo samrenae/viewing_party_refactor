7fa70b16a7fc4bce5b919b27c52e1fcba829adfd
Alma <almafernandez@yahoo.com>
2022-09-30 12:03:16 -0700

    new attemp - update person

--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,8 +1,8 @@
 class Person:
-    def __init__(self, name, watched, friends, subscriptions):
+    def __init__(self, name, watched, subscriptions):
         self.name = name
         self.watched = watched
-        self.friends = friends
+        self.friends = []
         self.subscriptions = subscriptions
         
     def add_watched(self, movie):
@@ -10,3 +10,7 @@ class Person:
     
     def get_num_watched(self):
         return len(self.watched)
+    
+    def add_friends(self,friend):
+        self.friends.append(friend)
+        
